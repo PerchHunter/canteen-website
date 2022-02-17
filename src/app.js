f0f4b8fc2881b79a42d5d0d0dc78286cc486fb2c
PerchHunter <bbldyk@mail.ru>
2022-02-17 06:38:45 +0500

    Почти завершил адаптив верхних блоков, сделал блок с логотипами, начал делать блок новостей

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 // import example from "./images/example.svg"; //импорт шрифтов и свг
 // import label from "./images/section-with-search-string/label.svg";
 
-import "./project/__commonStyles.scss";
+import "./project/commonStyles.scss";
 import "./project/header/header.scss";
 import "./project/header/telephone-dropdown/telephone-dropdown.scss";
 import "./project/upper-menu/upper-menu.scss";
 import "./project/upper-menu/category-dropdown-details/category-dropdown-details.scss";
 import "./project/catalog/catalog.scss";
 import "./project/catalog/catalog-dropdown/catalog-dropdown.scss";
-import "./project/section-with-slider/__slider/__slider.scss";
-import "./project/section-with-slider/__categories/dist/__categories.scss";
-import "./project/burger-menu/burger-menu.css";
+import "./project/section-with-slider/__sale/sale.scss";
+import "./project/section-with-slider/__slider/slider.scss";
+import "./project/section-with-slider/__categories/categories.scss";
+import "./project/burger-menu/burger-menu.scss";
+import "./project/header_mobile/header_mobile.scss";
+import "./project/brands/brands.scss";
+import "./project/news/news.scss";
 
 import "./project/header/header.js";
 import "./project/header/telephone-dropdown/telephone-dropdown.js";
@@ -19,3 +23,4 @@ import "./project/upper-menu/category-dropdown-details/category-dropdown-details
 import "./project/catalog/catalog-dropdown/catalog-dropdown.js";
 import "./project/section-with-slider/__slider/__slider.js";
 import "./project/burger-menu/burger-menu.js";
+import "./project/header_mobile/header_mobile.js";
